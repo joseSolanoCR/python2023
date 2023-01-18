88f5b72ed9506ca1b822ed35bb3b608687ef535b
jpsol <jsolanoca@ice.go.cr>
2023-01-18 11:54:55 -0600

    ciclos

---
+++ todoApp/for.py
@@ -0,0 +1,17 @@
+todos = []
+
+while True:
+    user_action = input("type add or show: ").strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for x in todos:
+                print(x.capitalize())
+        case 'exit':
+            break
+
+print('Bye')
+
