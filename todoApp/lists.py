584c139434d98b46ee33b917af90e26b232e203e
jpsol <jsolanoca@ice.go.cr>
2023-01-19 16:46:01 -0600

    todo improvements

--- todoApp/lists.py
+++ todoApp/lists.py
@@ -1,17 +1,18 @@
 todos = []
 
 while True:
-    user_action = input("type add, edit or show: ")
+    user_action = input("type add, edit, complete or show: ")
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for index, x in enumerate(todos):
+                print(f"{index}. {x}")
         case 'edit':
             for index, x in enumerate(todos):
-                print(index,x)
+                print(f"{index}. {x}")
             item = todos.index(input("What todo do you want to edit?"))
             n_item = input("New todo: ")
             todos[item] = n_item
@@ -19,6 +20,13 @@ while True:
             print(todos)
         case 'exit':
             break
+        case 'complete':
+            for index, x in enumerate(todos):
+                print(f"{index}. {x}")
+            cTodo = input("What todo do you want to complete?")
+            todos.remove(cTodo)
+            # todos.append(n_item)
+            print(todos)
 
 print('Bye')
 
